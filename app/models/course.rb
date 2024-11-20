cddbc2de3a1ac1cb9b18c7dab38ce16da0ee95d3
Tyler Estell <108036777+Testell@users.noreply.github.com>
2024-11-20 00:20:05 +0000

    refactored course model to account for degree requirements, db:migrate

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,18 +2,24 @@
 #
 # Table name: courses
 #
-#  id           :bigint           not null, primary key
-#  code         :string
-#  credit_hours :string
-#  name         :string
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
-#  school_id    :integer
+#  id              :bigint           not null, primary key
+#  code            :string
+#  course_category :string
+#  course_number   :integer
+#  credit_hours    :string
+#  name            :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  school_id       :integer
 #
 class Course < ApplicationRecord
   belongs_to :school, required: true, class_name: "School", foreign_key: "school_id"
+
   has_many :course_requirements, dependent: :destroy
   has_many :degree_requirements, through: :course_requirements
   has_many :start_transferable_courses, class_name: "TransferableCourse", foreign_key: "from_course_id", dependent: :destroy
   has_many :end_transferable_courses, class_name: "TransferableCourse", foreign_key: "to_course_id", dependent: :destroy
+
+  validates :course_number, presence: true, numericality: { only_integer: true }
+  validates :course_category, presence: true
 end
--- app/models/course_requirement.rb
+++ app/models/course_requirement.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: course_requirements
+#
+#  id                    :bigint           not null, primary key
+#  is_mandatory          :boolean          default(FALSE), not null
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#  course_id             :bigint           not null
+#  degree_requirement_id :bigint           not null
+#
+# Indexes
+#
+#  index_course_requirements_on_course_id              (course_id)
+#  index_course_requirements_on_degree_requirement_id  (degree_requirement_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (course_id => courses.id)
+#  fk_rails_...  (degree_requirement_id => degree_requirements.id)
+#
 class CourseRequirement < ApplicationRecord
   belongs_to :course, required: true, class_name: "Course", foreign_key: "course_id"
   belongs_to :degree_requirement, required: true, class_name: "DegreeRequirement", foreign_key: "degree_requirement_id"
