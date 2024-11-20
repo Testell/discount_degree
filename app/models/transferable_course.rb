5073d69950a57b7df64ac77492af0c7cb8514d7a
Tyler Estell <108036777+Testell@users.noreply.github.com>
2024-11-20 17:58:47 +0000

    updated transfercourses model through migrate, and updated file with correct validations and associations

--- app/models/transferable_course.rb
+++ app/models/transferable_course.rb
@@ -2,15 +2,16 @@
 #
 # Table name: transferable_courses
 #
-#  id                    :bigint           not null, primary key
-#  created_at            :datetime         not null
-#  updated_at            :datetime         not null
-#  degree_requirement_id :integer
-#  from_course_id        :integer
-#  to_course_id          :integer
+#  id             :bigint           not null, primary key
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  from_course_id :integer
+#  to_course_id   :integer
 #
 class TransferableCourse < ApplicationRecord
-  belongs_to :degree_requirement, required: true, class_name: "DegreeRequirement", foreign_key: "degree_requirement_id"
   belongs_to :from_course, required: true, class_name: "Course", foreign_key: "from_course_id"
   belongs_to :to_course, required: true, class_name: "Course", foreign_key: "to_course_id"
+
+  validates :from_course, presence: true
+  validates :to_course, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_20_001703) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_20_175621) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,7 +65,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_20_001703) do
   create_table "transferable_courses", force: :cascade do |t|
     t.integer "from_course_id"
     t.integer "to_course_id"
-    t.integer "degree_requirement_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
