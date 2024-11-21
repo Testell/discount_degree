47d0842990afaba07f7f73b61dff5433fe41b03a
Tyler Estell <108036777+Testell@users.noreply.github.com>
2024-11-21 02:13:39 +0000

    added pundit gem, bundle install, included pundit in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
+  include Pundit::Authorization
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
@@ -8,3 +9,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, :keys => [:avatar_url])
   end
 end
+
