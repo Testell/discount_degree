695cb923338992bd294f532651f65eaec98e5478
Tyler Estell <108036777+Testell@users.noreply.github.com>
2024-11-21 16:07:37 +0000

    added degree_requirement policy and added before_action to controller

---
+++ app/policies/degree_requirement_policy.rb
@@ -0,0 +1,35 @@
+class DegreeRequirementPolicy < ApplicationPolicy
+  def index?
+    admin? 
+  end
+
+  def show?
+    admin?
+  end
+
+  def new?
+    admin?
+  end
+
+  def edit?
+    admin?
+  end
+
+  def create?
+    admin?
+  end
+
+  def update?
+    admin?
+  end
+
+  def destroy?
+    admin?
+  end
+
+  class Scope < Scope
+    def resolve
+      admin? ? scope.all : scope.none
+    end
+  end
+end
