1646ff681adc8b751d78b4509c3331d3bdbc58e9
Tyler Estell <108036777+Testell@users.noreply.github.com>
2024-11-21 01:13:42 +0000

    standard devise set up, but added username and role

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,28 @@
 #
 # Table name: users
 #
-#  id         :bigint           not null, primary key
-#  email      :string
-#  password   :string
-#  role       :string
-#  username   :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                     :bigint           not null, primary key
+#  email                  :string
+#  encrypted_password     :string           default(""), not null
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  role                   :string
+#  username               :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: true
+  validates :role, presence: true, inclusion: { in: %w[user admin] }
 end
---
+++ db/migrate/20241121010330_add_devise_to_users.rb
@@ -0,0 +1,45 @@
+class AddDeviseToUsers < ActiveRecord::Migration[7.1]
+  def self.up
+    change_table :users, bulk: true do |t|
+      # Replace 'password' with 'encrypted_password' for Devise
+      t.remove :password if column_exists?(:users, :password)
+      t.string :encrypted_password, null: false, default: "" unless column_exists?(:users, :encrypted_password)
+
+      ## Recoverable
+      t.string   :reset_password_token unless column_exists?(:users, :reset_password_token)
+      t.datetime :reset_password_sent_at unless column_exists?(:users, :reset_password_sent_at)
+
+      ## Rememberable
+      t.datetime :remember_created_at unless column_exists?(:users, :remember_created_at)
+
+      ## Trackable
+      # Uncomment the following lines if you want to enable trackable functionality
+      # t.integer  :sign_in_count, default: 0, null: false
+      # t.datetime :current_sign_in_at
+      # t.datetime :last_sign_in_at
+      # t.string   :current_sign_in_ip
+      # t.string   :last_sign_in_ip
+
+      ## Confirmable
+      # Uncomment the following lines if you want to enable confirmable functionality
+      # t.string   :confirmation_token
+      # t.datetime :confirmed_at
+      # t.datetime :confirmation_sent_at
+      # t.string   :unconfirmed_email # Only if using reconfirmable
+
+      ## Lockable
+      # Uncomment the following lines if you want to enable lockable functionality
+      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      # t.string   :unlock_token # Only if unlock strategy is :email or :both
+      # t.datetime :locked_at
+    end
+
+    # Add indexes
+    add_index :users, :email, unique: true unless index_exists?(:users, :email)
+    add_index :users, :reset_password_token, unique: true unless index_exists?(:users, :reset_password_token)
+  end
+
+  def self.down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
