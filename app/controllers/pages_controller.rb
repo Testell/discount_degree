fd8568cc25237a43508ac913f3eee0b97ec9508d
Testell <108036777+Testell@users.noreply.github.com>
2024-12-09 08:52:49 -0600

    massive style change

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,28 +8,39 @@ class PagesController < ApplicationController
   def plan_page
     @degrees = Degree.all
     @community_colleges = School.where(school_type: 'community_college')
-    @universities = School.where(school_type: 'university')
+    @universities = School.where(id: Plan.select(:ending_school_id).distinct)
   end
 
   def generate_plan
     degree_id = params[:degree_id]
     starting_school_id = params[:starting_school_id]
     ending_school_id = params[:ending_school_id]
-    @plan = Plan.find_by(
+    
+    @plans = Plan.where(
       degree_id: degree_id,
       starting_school_id: starting_school_id,
       ending_school_id: ending_school_id
-    )
-
-    if @plan
-      redirect_to show_plan_path(plan_id: @plan.id)
+    ).order(total_cost: :desc)
+    
+    if @plans.any?
+      redirect_to show_plan_path(
+        plan_id: @plans.first.id, 
+        alternate_plan_id: @plans.second&.id
+      )
     else
-      flash[:alert] = 'No plan found for the selected options.'
+      flash[:alert] = 'No plans found for the selected options.'
       redirect_to plan_page_path
     end
   end
-
+  
   def show_plan
     @plan = Plan.find(params[:plan_id])
+    @alternate_plan = Plan.find(params[:alternate_plan_id]) if params[:alternate_plan_id]
+    @showing_full_time = params[:show_full_time] == 'true'
+  
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
